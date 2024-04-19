a8bd5da09d660c1594598a3df04c4489f0dcffa2
JhoanSB0608 <156533638+JhoanSB0608@users.noreply.github.com>
2024-04-19 14:26:49 -0500

    feat: :sparkles: Estructura ejercicios javascript

--- js/module/sumarRangoDeNumeros.js
+++
@@ -1,7 +0,0 @@
-export default sumarRango = (numeroIncial, numeroFinal)=>{
-    let resultado = 0;
-    for (let i = numeroIncial; i <= numeroFinal; i++) {
-        resultado += i
-    }
-    return resultado; 
-} 
--- js/module/traducirNumerosAPalabras.js
+++
@@ -1,7 +0,0 @@
-let lista = []
-let badera = undefined
-
-do {
-    lista.push(Number(await prompt("Ingrese un numero: ")))
-    badera = Number(await prompt("¿Deseas ingresar otro numero?"))
-}while 
