18e9f730bfa1731f04aaa84a37ade7807ee79807
JhoanSB0608 <156533638+JhoanSB0608@users.noreply.github.com>
2024-04-19 14:31:35 -0500

    feat: :sparkles: Se realizo el ejercicio 8

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import prompt from 'async-prompt';
-import {sumarRango} from "./module/sumarRangoDeNumeros.js"
+import {sumarRango} from "./modulo/sumarRangodeNumero.js"; 
 
-let num1 = Number(await prompt ("Ingrese el numero 1: "))
-let num2 = Number(await prompt ("Ingrese el numero 2: "))
+let num1 = Number(await prompt ("Ingrese el número 1: ") );
+let num2 = Number(await prompt ("Ingrese el número 2: ") );
 
 console.log(sumarRango(num1, num2));
