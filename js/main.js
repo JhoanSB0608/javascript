691b94441bad99cdbde12851d12f6fed0d919e1a
JhoanSB0608 <156533638+JhoanSB0608@users.noreply.github.com>
2024-04-22 21:57:50 -0500

    feat: :white_check_mark: Se realizo el ejercicio 10

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import prompt from 'async-prompt';
-import {sumarRango} from "./module/sumarRangoDeNumeros.js"
+import {numeroDeCaracteres} from "./module/numeroDeCaracteres.js"
 
-let num1 = Number(await prompt ("Ingrese el numero 1: "))
-let num2 = Number(await prompt ("Ingrese el numero 2: "))
-
-console.log(sumarRango(num1, num2));
+const cadena = await prompt("Por favor, ingrese una cadena: ");
+    const caracter = await prompt("Por favor, ingrese un carácter: ");
+    console.log("Número de veces que aparece el carácter:", numeroDeCaracteres(cadena, caracter));
