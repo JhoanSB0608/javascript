7c1d138a0b4c5ae7c722961119388fb353f98232
JhoanSB0608 <156533638+JhoanSB0608@users.noreply.github.com.>
2024-05-11 21:01:27 -0500

    feat: :white_check_mark: Multi-table queries are finished external composition of the product module

--- js/module/product.js
+++ js/module/product.js
@@ -10,6 +10,39 @@ export const getAllOrnamentalsProducts = async () => {
     return dataUpdate;
 }
 
+//-------------------------------------------------------------------------------------------------
+
+//8. Devuelve un listado de los productos que nunca han aparecido en un pedido.
+export const getAllProductsThatNeverHasBeenRequested = async()=>{
+    let res = await fetch("http://localhost:5506/products")
+    let data = await res.json();
+    let dataUpdate = [];
+    for(let i=0;i<data.length;i++){
+        let [ request_details ] = await getAllRequestDetailsByCode(data[i].code_product);
+        if(request_details===undefined) dataUpdate.push(data[i]);
+    }
+    return dataUpdate;
+}
+
+//9. Devuelve un listado de los productos que nunca han aparecido en un pedido.
+//El resultado debe mostrar el nombre, la descripción y la imagen del producto.
+export const getAllProductsThatNeverHasBeenRequestedWithItsNDI = async()=>{
+    let res = await fetch("http://localhost:5506/products")
+    let data = await res.json();
+    let dataUpdate = [];
+    for(let i=0;i<data.length;i++){
+        let [ request_details ] = await getAllRequestDetailsByCode(data[i].code_product);
+        if(request_details===undefined){
+            dataUpdate.push({
+                name: data[i].name,
+                description: data[i].description,
+                image: data[i].image,
+            });
+        }
+    }
+    return dataUpdate;
+}
+
 //Obtener data de un producto mediante su codigo
 export const getProductByCode = async (code = '') => {
     let res = await fetch(`http://localhost:5507/products?code_product=${code}`)
